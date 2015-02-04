85972df02cff5746434a18cf04fec870dca63b79
Karthick Kumar <karthick@ajsquare.net>
2015-02-04 15:33:56 +0530

    Update Security.php
    Security issues fixed

--- Bin/Security.php
+++ Bin/Security.php
@@ -19,63 +19,254 @@
 *
 */
 
-
 /**
- * This class contains functions related to  security process for sql injection
+ * This class contains functions related error hander
  *
- * @package  		Bin
+ * @package  		Bin_Core_Assembler
  * @author    		AjSquareInc Dev Team
  * @link   		http://www.zeuscart.com
- * @copyright 		Copyright (c) 2008 - 2013, AjSquare, Inc.
- * @link    		http://www.ajsquare.com/ajhome.php
  * @version  		Version 4.0
- * @created   		January 15 2013	
+ * @created   		January 15 2013
+ * @copyright 		Copyright (c) 2008 - 2013, AjSquare, Inc.
  */
-class Bin_Security
+class Bin_Core_Assembler
 {
-	/**
-	 * Check query for SQL injection
-	 *
-	 * @param array $fields
-	 * @return array
-	 */
-	function makeQuery($sql, $fields = array())
+	
+	private $do;
+		
+	public function __construct()
 	{
-		$sql = split("#",$sql);
-		$conv_sql = "";
-		if(count($fields)>0)
-		{
-			foreach($fields as $key=>$item)
-				$fields[$key] = mysql_escape_string(stripslashes($item));
-			$fields[count($fields)] = "";
-			foreach($sql as $key=>$item)
-			{
 
-				$conv_sql .= $item.$fields[key($fields)];
-				next($fields);
+		Bin_Core_Assembler::PowerSecurity();
+		include(ROOT_FOLDER.'Built/'.CURRENT_FOLDER."/Dll.php");	
+		if(isset($_GET['action']) && isset($_GET['action']{1}))
+			$this->do = trim($_GET['do']).':'.trim($_GET['action']);
+		else
+			$this->do = trim($_GET['do']);
+
+			
+		if(array_key_exists($this->do,$domapping))
+		{
+			if(!(int)$domapping[$this->do]['loadlib'])
+			{
+				$this->loadModelFiles($domapping, $globalmapping);			
+			}
+			else
+			{
+				$this->loadSystemFiles($system);	
+				$this->loadLibrayFiles($libraries);				
+				$this->loadModelFiles($domapping, $globalmapping);			
 			}
 		}
-		return trim($conv_sql);
-	}
-
-	 
-	function checkConfigFile()
-	{
-
-		if(file_exists(ROOT_FOLDER.'Bin/Configuration.php'))
-		{
-			include(ROOT_FOLDER.'Bin/Configuration.php');
-			if(class_exists("Bin_Configuration"))
-				return true;
-			else
-				return false;
-		}
 		else
-			return false;
-	} 
+		{
+			if(!(int)$globalmapping['invalidrequest']['loadlib'])
+			{
+				$this->loadModelFiles($domapping, $globalmapping);			
+				
+			}
+			else
+			{
+				$this->loadSystemFiles($system);	
+				$this->loadLibrayFiles($libraries);				
+				$this->loadModelFiles($domapping, $globalmapping);			
+			}		
+		}
+	}
+	
+	
+	private function loadSystemFiles($system)
+	{
+		foreach($system as $key=>$item)	
+			include(ROOT_FOLDER.$item);	
+	}
+	
+	
+	
+	private function loadLibrayFiles($library)
+	{
+		foreach($library as $key=>$item)	
+			include(ROOT_FOLDER.$item);	
+	}
+	
+	private function loadModelFiles($domapping,$globalmapping)
+	{
+		if(array_key_exists($this->do,$domapping))
+		{
+			include_once('classes/Model/'.$domapping[$this->do]['model'].'.php');
+			$class = "Model_".$domapping[$this->do]['model'];			
+			$function = $domapping[$this->do]['function'];
+			$obj = new $class;
+			$obj->$function();			
+		}
+		else 
+		{
+			include_once('classes/Model/'.$globalmapping['invalidrequest']['model'].'.php');
+			$class = "Model_".$globalmapping['invalidrequest']['model'];					
+			$function = $globalmapping['invalidrequest']['function'];		
+			$obj = new $class;	
+			$obj->$function();
+		}
+	}	
+	
+	// function PowerSecurity()
+	// {
+	// 	//	Power security in $_POST single value and array values
+	// 	//	******************************************************
+		
+	// 	foreach($_POST as $key=>$itemm)
+	// 	{
+	
+	// 		if($key != "google_script")
+	// 		{
+	// 			if(!is_array($itemm))
+	// 			{
+	// 				if(!strstr($itemm,"<script"))
+	// 				{
+	// 					$_POST[$key] = mysql_escape_string(stripslashes(substr($itemm,strpos($itemm,"</script>"),strlen($itemm))));
+	// 				}
+	// 				else
+	// 				{
+	// 					$_POST[$key] = "";
+	// 				}
+	// 			}
+	// 			else
+	// 			{
+	// 				$_POST[$key] = Bin_Core_Assembler::recuresiveArray($itemm);
+	// 			}
+	// 		}
+	// 	}	
+		
+	// 	//	Power security in $_GET single value and array values
+	// 	//	*****************************************************
+	// 	foreach($_GET as $key=>$itemm)
+	// 	{
+	// 		if(!is_array($itemm))
+	// 		{
+	// 			if(!strstr($itemm,"<script"))
+	// 			{
+	// 				$_GET[$key] = mysql_escape_string(stripslashes(substr($itemm,strpos($itemm,"</script>"),strlen($itemm))));
+	// 			}
+	// 			else
+	// 			{
+	// 				$_GET[$key] = "";
+	// 			}
+				
+	// 		}
+	// 		else
+	// 		{
+	// 			$_GET[$key] = Bin_Core_Assembler::recuresiveArray($itemm);
+	// 		}
+	// 	}
+		
+	// }
+
+	public function PowerSecurity()
+	{		
+
+		if($_REQUEST['do']!='captcha'){
+
+		foreach ($_POST as $key => $value) {
+			if($key != "description")
+			{
+				$_POST[$key]=filter_var($value, FILTER_SANITIZE_STRING);
+
+			}
+		}
+
+		foreach ($_GET as $key => $value) {
+			if($key != "description")
+			{
+				$_GET[$key]=filter_var($value, FILTER_SANITIZE_STRING);
+
+			}
+		}
+
+		foreach ($_REQUEST as $key => $value) {
+			if($key != "description")
+			{
+				$_REQUEST[$key]=filter_var($value, FILTER_SANITIZE_STRING);
+
+			}
+		}
+
+	}
+	}
+	
+	function recuresiveArray($itemm)
+	{
+ 
+		foreach($itemm as $key=>$item)
+		{
+			if(!is_array($item))
+			{
+				if(!strstr($item,"<script"))
+				{
+					$itemm[$key] = mysql_escape_string(stripslashes(substr($item,strpos($item,"</script>"),strlen($item))));
+				}
+				else
+				{
+					$itemm[$key] = "";
+				}
+				 
+			}
+			else
+			{
+				return Bin_Core_Assembler::recuresiveArray($item);
+			}
+		}
+		return $itemm;
+	}
+	
+	function PHPPowerSecurity()
+	{
+		//	Power security in $_POST single value and array values
+		//	******************************************************
+		
+		foreach($_POST as $key=>$itemm)
+		{
+	
+			if(!is_array($itemm))
+			{
+				$_POST[$key] = mysql_escape_string(stripslashes($itemm));
+			}
+			else
+			{
+				$_POST[$key] = Bin_Core_Assembler::PHPrecuresiveArray($itemm);
+			}
+		}	
+		
+		//	Power security in $_GET single value and array values
+		//	*****************************************************
+		foreach($_GET as $key=>$itemm)
+		{
+			if(!is_array($itemm))
+			{
+				$_GET[$key] = mysql_escape_string(stripslashes($itemm));
+			}
+			else
+			{
+				$_GET[$key] = Bin_Core_Assembler::PHPrecuresiveArray($itemm);
+			}
+		}
+		
+	}
+	
+	function PHPrecuresiveArray($itemm)
+	{
+ 
+		foreach($itemm as $key=>$item)
+		{
+			if(!is_array($item))
+			{
+				$itemm[$key] = mysql_escape_string(stripslashes($item));				 
+			}
+			else
+			{
+				return Bin_Core_Assembler::PHPrecuresiveArray($item);
+			}
+		}
+		return $itemm;
+	}
 }
-
- if(!Bin_Security::checkConfigFile())
-	die("Configuration file corrupted or missing..."); 
-
 ?>
